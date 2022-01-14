2a4898ed17da7088845b0c4ac328b88e004d272f
FelipeMDantas <felipemdantas@terra.com.br>
2022-01-13 23:06:45 -0300

    colors definition

--- src/App.js
+++ src/App.js
@@ -10,13 +10,22 @@ const App = () => {
 
   return (
     <SafeAreaView>
-
+      <View style={styles.sectionView}>
+        <Text style={styles.sectionText}>Hey planet!</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  sectionView: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  sectionText: {
+    color: 'white',
+  },
 });
 
 export default App;

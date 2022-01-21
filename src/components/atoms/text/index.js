9cfb798cc009e6a758b158afe7fa93c498db4447
FelipeMDantas <felipemdantas@terra.com.br>
2022-01-20 23:16:43 -0300

    font components configuration completed

--- src/components/atoms/text/index.js
+++ src/components/atoms/text/index.js
@@ -5,3 +5,48 @@ export const Title = styled.Text`
     color: white;
     font-weight: bold;
 `;
+
+export const InputLabel = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(14)}px;
+    color: white;
+    font-weight: 600;
+`;
+
+export const DetailTitle = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(24)}px;
+    color: white;
+    font-weight: bold;
+`;
+
+export const DetailSubTitle = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(18)}px;
+    color: white;
+`;
+
+export const DetailText = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(14)}px;
+    color: white;
+`;
+
+export const DetailSectionTitle = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(20)}px;
+    color: white;
+    font-weight: bold;
+`;
+
+export const CardTitle = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(14)}px;
+    color: white;
+    font-weight: bold;
+`;
+
+export const CardDescription = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(10)}px;
+    color: white;
+`;
+
+export const CardHightLightText = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(16)}px;
+    color: white;
+    font-weight: 600;
+`;
--- src/components/atoms/text/index.stories.js
+++ src/components/atoms/text/index.stories.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import { text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { Title } from './index';
+import { Title, CardDescription, CardHightLightText, CardTitle, DetailSectionTitle, DetailSubTitle, DetailText, DetailTitle, InputLabel} from './index';
 
 const Wrapper = styled.View`
     flex: 1;
@@ -13,12 +13,49 @@ const Wrapper = styled.View`
 
 const stories = storiesOf('Text', module);
 
+stories.addDecorator((getStory) => <Wrapper>{getStory()}</Wrapper>);
+
 stories.add('Title', () => {
     const value = text('Title', 'Title Example');
-    return(
-        <Wrapper>
-            <Title>{value}</Title>
-        </Wrapper>
-    )
-})
+    return <Title>{value}</Title>
+});
 
+stories.add('CardTitle', () => {
+    const value = text('CardTitle', 'CardTitle Example');
+    return <CardTitle>{value}</CardTitle>
+});
+
+stories.add('CardDescription', () => {
+    const value = text('CardDescription', 'CardDescription Example');
+    return <CardDescription>{value}</CardDescription>
+});
+
+stories.add('CardHightLightText', () => {
+    const value = text('CardHightLightText', 'U$ 222.22');
+    return <CardHightLightText>{value}</CardHightLightText>
+});
+
+stories.add('DetailSectionTitle', () => {
+    const value = text('DetailSectionTitle', 'DetailSectionTitle');
+    return <DetailSectionTitle>{value}</DetailSectionTitle>
+});
+
+stories.add('DetailSubTitle', () => {
+    const value = text('DetailSubTitle', 'DetailSubTitle');
+    return <DetailSubTitle>{value}</DetailSubTitle>
+});
+
+stories.add('DetailText', () => {
+    const value = text('DetailText', 'DetailText');
+    return <DetailText>{value}</DetailText>
+});
+
+stories.add('DetailTitle', () => {
+    const value = text('DetailTitle', 'DetailTitle');
+    return <DetailTitle>{value}</DetailTitle>
+});
+
+stories.add('InputLabel', () => {
+    const value = text('InputLabel', 'InputLabel');
+    return <InputLabel>{value}</InputLabel>
+});

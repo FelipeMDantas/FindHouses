3cb1099dc50e3b5a8bd423a664937ffa7dcc9b0b
FelipeMDantas <felipemdantas@terra.com.br>
2022-01-21 23:52:54 -0300

    button components configuration initialized

--- src/components/atoms/text/index.js
+++ src/components/atoms/text/index.js
@@ -12,6 +12,12 @@ export const InputLabel = styled.Text`
     font-weight: 600;
 `;
 
+export const ButtonText = styled.Text`
+    font-size: ${({ theme }) => theme.metrics.px(22)}px;
+    color: white;
+    font-weight: bold;
+`;
+
 export const DetailTitle = styled.Text`
     font-size: ${({ theme }) => theme.metrics.px(24)}px;
     color: white;
---
+++ storybook/stories.js
@@ -0,0 +1,2 @@
+import '../src/components/atoms/text/index.stories';
+import '../src/components/atoms/button/index.stories';

fc8f19d842f994606d55b9b5f4fb373ca992d27e
FelipeMDantas <felipemdantas@terra.com.br>
2022-01-23 21:54:54 -0300

    input and placeholder stylization started

--- storybook/stories.js
+++ storybook/stories.js
@@ -1,3 +1,4 @@
 import '../src/components/atoms/text/index.stories';
 import '../src/components/atoms/button/index.stories';
 import '../src/components/atoms/iconButton/index.stories';
+import '../src/components/molecules/input/index.stories';
